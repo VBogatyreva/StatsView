554f4cef11146af248015d96e8b73145a5842eb4
Viktoria Bogatyreva <bogatyreva-va@mail.ru>
2025-04-29 15:40:29 +0300

    Animation. Part1

--- app/src/main/java/ru/netology/statsview/MainActivity.kt
+++ app/src/main/java/ru/netology/statsview/MainActivity.kt
@@ -3,21 +3,17 @@ package ru.netology.statsview
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        findViewById<StatsView>(R.id.statsView).data = listOf(
-            500F,
-            500F,
-            500F,
-            500F,
-        )
+
+        val view = findViewById<StatsView>(R.id.statsView)
+
+        view.postDelayed({
+            view.data = listOf(
+                500F, 500F, 500F, 500F
+            )
+        }, 300)
     }
 }
-
-
-
-
-
--- app/src/main/java/ru/netology/statsview/StatsView.kt
+++ app/src/main/java/ru/netology/statsview/StatsView.kt
@@ -1,12 +1,16 @@
 package ru.netology.statsview
 
+import android.animation.AnimatorSet
+import android.animation.ValueAnimator
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.PointF
 import android.graphics.RectF
 import android.util.AttributeSet
 import android.view.View
+import android.view.animation.AccelerateDecelerateInterpolator
 import androidx.core.content.withStyledAttributes
 import ru.netology.statsview.utils.AndroidUtils
 import kotlin.math.min
@@ -24,13 +28,17 @@ class StatsView @JvmOverloads constructor(
     defStyleRes
 ) {
     private var textSize = AndroidUtils.dp(context, 20).toFloat()
-    private var lineWidth = AndroidUtils.dp(context, 5)
+    private var lineWidth = AndroidUtils.dp(context, 5).toFloat()
     private var colors = emptyList<Int>()
 
+    private var rotationAngle = 0f
+    private var rotationAnimator: ValueAnimator? = null
+
     init {
+
         context.withStyledAttributes(attributeSet, R.styleable.StatsView) {
             textSize = getDimension(R.styleable.StatsView_textSize, textSize)
-            lineWidth = getDimension(R.styleable.StatsView_lineWidth, lineWidth.toFloat()).toInt()
+            lineWidth = getDimension(R.styleable.StatsView_lineWidth, lineWidth)
             colors = listOf(
                 getColor(R.styleable.StatsView_color1,generateRandomColor()),
                 getColor(R.styleable.StatsView_color2,generateRandomColor()),
@@ -38,12 +46,38 @@ class StatsView @JvmOverloads constructor(
                 getColor(R.styleable.StatsView_color4,generateRandomColor())
             )
         }
+        startAnimation()
+    }
+
+    private fun startAnimation() {
+        progressAnimator = ValueAnimator.ofFloat(0f, 1f).apply {
+            duration = 1000
+            interpolator = AccelerateDecelerateInterpolator()
+            addUpdateListener {
+                progress = it.animatedValue as Float
+                invalidate()
+            }
+        }
+
+        rotationAnimator = ValueAnimator.ofFloat(0f, 360f).apply {
+            duration = 1000
+            interpolator = AccelerateDecelerateInterpolator()
+            addUpdateListener {
+                rotationAngle = it.animatedValue as Float
+                invalidate()
+            }
+        }
+
+        AnimatorSet().apply {
+            playTogether(progressAnimator, rotationAnimator)
+            start()
+        }
     }
 
     var data: List<Float> = emptyList()
         set(value) {
             field = value
-            invalidate()
+            update()
         }
 
     private val countedData: List<Float>
@@ -55,13 +89,15 @@ class StatsView @JvmOverloads constructor(
         }
 
     private var radius = 0F
-    private var center = PointF()
-    private var oval = RectF()
+    private var center = PointF(0F,0F)
+    private var oval = RectF(0F,0F,0F,0F,)
+    private var progress = 0F
+    private var progressAnimator: ValueAnimator? = null
 
     private val paint = Paint(
         Paint.ANTI_ALIAS_FLAG
     ).apply {
-        strokeWidth = lineWidth.toFloat()
+        strokeWidth = this@StatsView.lineWidth
         style = Paint.Style.STROKE
         strokeJoin = Paint.Join.ROUND
         strokeCap = Paint.Cap.ROUND
@@ -72,6 +108,7 @@ class StatsView @JvmOverloads constructor(
         textSize = this@StatsView.textSize
         style = Paint.Style.FILL
         textAlign = Paint.Align.CENTER
+        color = Color.BLACK
     }
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
@@ -86,16 +123,20 @@ class StatsView @JvmOverloads constructor(
     }
 
     override fun onDraw(canvas: Canvas) {
-        if (data.isEmpty()) {
-            return
-        }
+        if (data.isEmpty()) return
         val counted = countedData
+        if (counted.isEmpty()) return
+
+        canvas.save()
+
+        canvas.rotate(rotationAngle, center.x, center.y)
 
         var startAngle = -90F
+
         counted.forEachIndexed { index, datum ->
             val angle = datum * 360F
             paint.color = colors.getOrElse(index) {generateRandomColor()}
-            canvas.drawArc(oval, startAngle, angle, false, paint)
+            canvas.drawArc(oval, startAngle, angle * progress, false, paint)
             startAngle += angle
         }
 
@@ -107,12 +148,34 @@ class StatsView @JvmOverloads constructor(
             startAngle += angle
         }
 
+        canvas.restore()
+
         canvas.drawText(
-            "%.2f%%".format(counted.sum() * 100),
+            "%.2f%%".format(counted.sum() * 100 * progress),
             center.x,
             center.y + textPaint.textSize / 4,
             textPaint,
         )
     }
+    private fun update() {
+        progressAnimator?.let {
+            it.removeAllListeners()
+            it.cancel()
+        }
+        progress = 0F
+        progressAnimator = ValueAnimator.ofFloat(0F, 1F).apply {
+            addUpdateListener { anim ->
+                progress = anim.animatedValue as Float
+                invalidate()
+            }
+            duration = 1000
+            interpolator = AccelerateDecelerateInterpolator()
+        }.also {
+            it.start()
+        }
+    }
+
     private fun generateRandomColor() = Random.nextInt(0xFF000000.toInt(), 0xFFFFFFFF.toInt())
 }
+
+
